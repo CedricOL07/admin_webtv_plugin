825bcb43c14797e72fb3976a6d2c95f091cf2285
cedricOL07 <cedric.olivero@orange.fr>
2017-04-03 20:01:21 +0200

    remise en place du graphique avec les sliders

--- includes/nouveaux_reglages/index.php
+++ includes/nouveaux_reglages/index.php
@@ -3,7 +3,6 @@
         <meta charset="utf-8">
         <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
         <title>WEBTVFIL</title>
 
         <!--<style type="text/css">
