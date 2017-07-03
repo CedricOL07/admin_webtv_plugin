12c9d3788d24e2c0ccbfde524d8b8261f853239d
louis2580 <louis0710@live.fr>
2017-07-03 19:33:02 +0200

    traitement js -> php

--- includes/WEBTV/traitement_duree_clips.php
+++ includes/WEBTV/traitement_duree_clips.php
@@ -16,27 +16,28 @@ Appel des différentes fonctions du programme
 //add_action( 'wp_ajax_traitement_infos_nouveaux_reglages', 'traitement_infos_nouveaux_reglages' );
 add_action('wp_ajax_recuperer_duree_clip','recuperer_duree_clip');
 
-function recuperer_duree_clip(){
-    $nom_clip;
-    $url_clip;
+function recuperer_duree_clip($url_clip, $nom_clip){
+    $nom = $nom_clip;
+    $url = $url_clip;
+    /*
     if(isset($_POST['nom_clip']) && isset($_POST['url_clip'])){
       	$nom_clip=$_POST['nom_clip'];
       	$url_clip=$_POST['url_clip'];
    	} else {
    		wp_die();
    	}
-
-   	$filename = $url_clip;
+    */
+   	$filename = $url;
 
    	if (strpos($filename, "//"))
    	{
    		
-   		$domaine = __DIR__;
-		$deb = strpos($domaine, "wordpress"); 
-		$domaine = substr($domaine, 0, $deb);		// Récupère le chemin local
-		$domaine = str_replace('\\', '/', $domaine);
-		$fin = strpos($filename, "wordpress"); 
-		$filename = $domaine . substr($filename, $fin);
+     	$domaine = __DIR__;
+  		$deb = strpos($domaine, "wordpress"); 
+  		$domaine = substr($domaine, 0, $deb);		// Récupère le chemin local
+  		$domaine = str_replace('\\', '/', $domaine);
+  		$fin = strpos($filename, "wordpress"); 
+  		$filename = $domaine . substr($filename, $fin);
    		/**/
    	}
 
